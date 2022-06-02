cb1c7ae30a0d1354bd21fbe7a9461d3d0bbdb45a
ramana <ramanamurthy.bure@gmail.com>
2022-06-02 14:42:36 +0530

    Updated the latest files

--- pythonUdemyCourse/Basics/13_IOFiles.py
+++ pythonUdemyCourse/Basics/13_IOFiles.py
@@ -2,13 +2,13 @@
 # Before we finish this section,let's quickly go over how to perform simple I/O with basic.txt files
 # We will also discuss file paths on your computer. Let's get started.
 print("######################  (1) ############################################")
-file =open("C://Users//raman//PycharmProjects//pythonCore//Resources//Sample.txt","w") # open for writing
+file =open("/Resources/Sample.txt", "w") # open for writing
 file.write("Hello this is a text file")
 file.write("\nThis is a second line")
 file.write("\nThis is a third line")
 file.close()
 print("######################  (2) ############################################")
-file =open("C://Users//raman//PycharmProjects//pythonCore//Resources//Sample.txt","r") # open for reading
+file =open("/Resources/Sample.txt", "r") # open for reading
 # When we read() the file the cursor go all the way to the end of the file.
 # So we need to reset the cursor to get the text again
 print("Reading for the first time :",file.read()) # This will give the all the text in one string
@@ -41,7 +41,7 @@ print("######################  (6) ############################################"
 # If we want to open files at another location on your computer,simply pass in the entire file path.
 # For windows you need to use double \ so python doesn't treat the second \ as an escape character, a file
 # path is in the form:
-myfile = open("C://Users//raman//PycharmProjects//pythonCore//Resources//Sample.txt")
+myfile = open("/Resources/Sample.txt")
 print("######################  (7) ############################################")
 # For MacOS and Linux you use slashes in the opposite direction:
 myfile = open("/Users/raman/PycharmProjects/pythonCore/Resources/Sample.txt")
---
+++ pythonUdemyCourse/Modules/45_ModuleAndPackages.py
@@ -0,0 +1,19 @@
+# PyPi is a repository for open-source third-party python packages
+# It's similar to RubyGems in Ruby world, PHP's packagist, CPAN for Perl, and NPM for Node.js
+# So far we've really only used libraries that come internally with Python
+# There are many other libraries available that people have open-sourced and shared on PyPi.
+# We can use pip install at the command line to install these pakages
+# pip is a simple way to download packages at your command line directly from the PyPi repository
+# There are packages already created for almost any use case you can think of!
+# A quick google search will usually help you discover a link to the PyPi page for the package,
+# or for the package documentation
+
+# import openpyxl -> TO work with the excel in python
+
+# Modules are just .py scripts that you call in another .py script
+# Packages are a collection of modules
+# Let's create some examples
+
+
+
+
