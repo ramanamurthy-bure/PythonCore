71d030d9daa41b8b3c0862153e047afb2b1ecbd2
ramana <ramanamurthy.bure@gmail.com>
2022-06-08 22:32:14 +0530

    Updated the latest files

---
+++ PythonWebScraping/1_WebScraping.py
@@ -0,0 +1,5 @@
+# Web Scraping is a general term for techniques involving automating the gathering of data from a website
+# In this section we will learn how to use Python to conduct web scraping tasks such as downloading the images
+# or information off a website
+
+
